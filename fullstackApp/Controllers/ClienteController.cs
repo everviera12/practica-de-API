ef0610c2447735e77852402f62310f4873e7bdce
EverViera12 <evertigre10@gmail.com>
2024-02-18 19:03:19 -0600

    Agregar archivos de proyecto.

---
+++ fullstackApp/Controllers/ClienteController.cs
@@ -0,0 +1,60 @@
+﻿using fullstackApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace fullstackApp.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public class ClientesController : ControllerBase
+    {
+        // obtener todos los clientes
+        [HttpGet]
+        [Route("/lista")]
+        public dynamic ListarClientes()
+        {
+            List<Cliente> clientes = new List<Cliente>
+            {
+                // usuario 1
+                new Cliente
+                {
+                    id = "1",
+                    nombre = "Victor Viera",
+                    edad = "23",
+                    email = "everisc2@gmail.com"
+                },
+
+                // usuario 2
+                new Cliente
+                {
+                    id = "2",
+                    nombre = "Victor Viera",
+                    edad = "23",
+                    email = "everisc2@gmail.com"
+                }
+            };
+            return clientes;
+        }
+
+        // obtener cliente por ID
+        [HttpGet]
+        [Route("listaID")]
+        public dynamic ListarClientePorID(int codigo)
+        {
+            return new Cliente
+            {
+                id = codigo.ToString(),
+                nombre = "Victor Viera",
+                edad = "23",
+                email = "everisc2@gmail.com"
+            };
+        }
+
+        [HttpPost]
+        [Route("/guardar")]
+        public dynamic GuardarCliente([FromBody] Cliente cliente)
+        {
+            return cliente;
+        }
+    }
+}  
---
+++ fullstackApp/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace fullstackApp.Models
+{
+    public class Cliente
+    {
+        public string id{ get; set; }
+        public string nombre {get; set;}
+        public string edad { get; set; }
+        public string email { get; set; }
+
+    }
+}
